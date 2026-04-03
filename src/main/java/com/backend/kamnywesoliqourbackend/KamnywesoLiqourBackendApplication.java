1a54f8c47a29b76b1de6ecdc6996671d0eccb419
fawzan saeed <fauzdasoodais@gmail.com>
2026-04-04 00:35:02 +0300

    initializing springboot backend

---
+++ src/main/java/com/backend/kamnywesoliqourbackend/KamnywesoLiqourBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.kamnywesoliqourbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamnywesoLiqourBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamnywesoLiqourBackendApplication.class, args);
+    }
+
+}
